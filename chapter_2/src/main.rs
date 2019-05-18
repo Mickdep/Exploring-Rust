e2719f4c743c3ba72d373673b92cceed67f58878
Mickdep <mdepeinder@gmail.com>
2019-05-18 13:10:41 +0200

    Chapter_2 string guess.

--- chapter_2/src/main.rs
+++ chapter_2/src/main.rs
@@ -1,14 +1,35 @@
 use std::io;
-
 fn main() {
     println!("Welcome to my guesing game.");
-    println!("Input a number to guess: ");
+    println!("Input a string of 3 characters to guess: ");
 
-    let mut guess = String::new();
+    let mut guess = get_user_input();
     //  ^^^ mut keyword to make this variable mutable
+    //In this case it's there to actually assign it from read_line
+    
+    while guess.len() != 3 {
+        println!("Please enter a maximum of 3 characters");
+        guess = get_user_input();
+    }
 
-    io::stdin().read_line(&mut guess).expect("Failed to read line.");
+    //Passes the guess String to check_guess(), thus transferring ownership. check_guess() frees the memory.
+    check_guess(guess);
+}
 
-    println!("You guessed {}", guess);
+//Takes String as argument so it takes ownership of the string
+fn check_guess(guess: String){
+    let value_to_guess = "adj";
+    println!("Checking guess: {}", guess);
+    if guess == value_to_guess {
+        println!("You guessed correctly!");
+    }else{
+        println!("You guessed incorrectly! Shutting down...");
+    }
+}
 
+fn get_user_input() -> String{
+    let mut input = String::new(); //Create string to store input in
+    io::stdin().read_line(&mut input).expect("Failed to read line."); //Read input and store it in input
+
+    return String::from(input.trim_end()); //Trim the trailing \r\n
 }
