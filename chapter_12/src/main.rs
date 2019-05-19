25fbe1253cca6988c1c7cf478d013fa81816ede9
Mickdep <mdepeinder@gmail.com>
2019-05-19 23:18:48 +0200

    Completed the I/O program

--- chapter_12/src/main.rs
+++ chapter_12/src/main.rs
@@ -1,3 +1,17 @@
+use std::env;
+use std::process;
+
+use chapter_12::Config;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect(); //Get command line args
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = chapter_12::run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    }
 }
